c7f1e6db7f419418c14020b7adbf3c57d9835676
odybieneuene <ody.bieneugene.com>
2023-10-28 02:21:58 -0400

    bloomm

---
+++ scripts/hamburger.js
@@ -0,0 +1,7 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
